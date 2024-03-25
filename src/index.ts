1917c6eea385866f78f68aded54b939205499394
mycoin <1845440+mycoin@users.noreply.github.com>
2024-03-25 18:04:48 +0800

    Dist

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import path from 'path'
+import path from "path";
 
 export default {
-  parser: 'babel-eslint',
-  extends: ['eslint:recommended', 'airbnb'],
-  plugins: ['import'],
+  parser: "babel-eslint",
+  extends: ["eslint:recommended", "airbnb"],
+  plugins: ["import"],
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
@@ -18,66 +18,68 @@ export default {
     es6: true,
   },
   globals: {
+    define: true,
     jQuery: true,
     F: true,
   },
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       alias: {
-        map: [['@', path.resolve('./src/')]],
-        extensions: ['', '.js', '.jsx', '.ts', '.tsx'],
+        map: [["@", path.resolve("./src/")]],
+        extensions: ["", ".js", ".jsx", ".ts", ".tsx"],
       },
     },
   },
   rules: {
-    'import/no-unresolved': [
+    "import/no-unresolved": [
       2,
       {
-        ignore: ['^@weex-module/'],
+        ignore: ["^@weex-module/"],
       },
     ],
-    'arrow-body-style': 0,
-    'func-names': 0,
-    'max-len': [
-      'error',
+    "arrow-body-style": 0,
+    "func-names": 0,
+    "max-len": [
+      "error",
       {
         code: 140,
       },
     ],
-    'class-methods-use-this': 0,
-    'jsx-a11y/anchor-is-valid': 0,
-    'jsx-a11y/iframe-has-title': 0,
-    'no-else-return': 0,
-    'no-param-reassign': 0,
-    'no-plusplus': 0,
-    'no-nested-ternary': 0,
-    'no-unstable-nested-components': 0,
-    'no-restricted-globals': 0,
-    'object-curly-newline': 0,
-    'prefer-arrow-callback': 0,
-    'prefer-destructuring': 0,
-    'prefer-template': 0,
-    'quote-props': ['error', 'consistent-as-needed'],
-    semi: ['error', 'never'],
-    'newline-per-chained-call': [
-      'error',
+    "class-methods-use-this": 0,
+    "jsx-a11y/anchor-is-valid": 0,
+    "jsx-a11y/iframe-has-title": 0,
+    "no-else-return": 0,
+    "no-param-reassign": 0,
+    "no-plusplus": 0,
+    "no-nested-ternary": 0,
+    "no-unstable-nested-components": 0,
+    "no-restricted-globals": 0,
+    "object-curly-newline": 0,
+    "prefer-arrow-callback": 0,
+    "prefer-destructuring": 0,
+    "prefer-template": 0,
+    "quote-props": ["error", "consistent-as-needed"],
+    semi: ["error", "never"],
+    "newline-per-chained-call": [
+      "error",
       {
         ignoreChainWithDepth: 2,
       },
     ],
-    'react/function-component-definition': 0,
-    'react/destructuring-assignment': 0,
-    'react/forbid-prop-types': 0,
-    'react/jsx-closing-bracket-location': 0,
-    'react/jsx-indent': 'error',
-    'react/jsx-newline': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/jsx-one-expression-per-line': 0,
-    'react/no-deprecated': 0,
-    'react/prefer-stateless-function': 1,
-    'react/prop-types': 0,
-    'react/require-default-props': 0,
-    'react/require-extension': 0,
-    'react/sort-comp': 0,
+    "react/function-component-definition": 0,
+    "react/destructuring-assignment": 0,
+    "react/forbid-prop-types": 0,
+    "react/jsx-closing-bracket-location": 0,
+    "react/jsx-indent": "error",
+    "react/jsx-newline": 0,
+    "react/jsx-props-no-spreading": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "react/no-deprecated": 0,
+    "react/no-unused-class-component-methods": 1,
+    "react/prefer-stateless-function": 1,
+    "react/prop-types": 0,
+    "react/require-default-props": 0,
+    "react/require-extension": 0,
+    "react/sort-comp": 0,
   },
-}
+};
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -16,11 +16,12 @@ declare const _default: {
         es6: boolean;
     };
     globals: {
+        define: boolean;
         jQuery: boolean;
         F: boolean;
     };
     settings: {
-        'import/resolver': {
+        "import/resolver": {
             alias: {
                 map: string[][];
                 extensions: string[];
@@ -28,46 +29,47 @@ declare const _default: {
         };
     };
     rules: {
-        'import/no-unresolved': (number | {
+        "import/no-unresolved": (number | {
             ignore: string[];
         })[];
-        'arrow-body-style': number;
-        'func-names': number;
-        'max-len': (string | {
+        "arrow-body-style": number;
+        "func-names": number;
+        "max-len": (string | {
             code: number;
         })[];
-        'class-methods-use-this': number;
-        'jsx-a11y/anchor-is-valid': number;
-        'jsx-a11y/iframe-has-title': number;
-        'no-else-return': number;
-        'no-param-reassign': number;
-        'no-plusplus': number;
-        'no-nested-ternary': number;
-        'no-unstable-nested-components': number;
-        'no-restricted-globals': number;
-        'object-curly-newline': number;
-        'prefer-arrow-callback': number;
-        'prefer-destructuring': number;
-        'prefer-template': number;
-        'quote-props': string[];
+        "class-methods-use-this": number;
+        "jsx-a11y/anchor-is-valid": number;
+        "jsx-a11y/iframe-has-title": number;
+        "no-else-return": number;
+        "no-param-reassign": number;
+        "no-plusplus": number;
+        "no-nested-ternary": number;
+        "no-unstable-nested-components": number;
+        "no-restricted-globals": number;
+        "object-curly-newline": number;
+        "prefer-arrow-callback": number;
+        "prefer-destructuring": number;
+        "prefer-template": number;
+        "quote-props": string[];
         semi: string[];
-        'newline-per-chained-call': (string | {
+        "newline-per-chained-call": (string | {
             ignoreChainWithDepth: number;
         })[];
-        'react/function-component-definition': number;
-        'react/destructuring-assignment': number;
-        'react/forbid-prop-types': number;
-        'react/jsx-closing-bracket-location': number;
-        'react/jsx-indent': string;
-        'react/jsx-newline': number;
-        'react/jsx-props-no-spreading': number;
-        'react/jsx-one-expression-per-line': number;
-        'react/no-deprecated': number;
-        'react/prefer-stateless-function': number;
-        'react/prop-types': number;
-        'react/require-default-props': number;
-        'react/require-extension': number;
-        'react/sort-comp': number;
+        "react/function-component-definition": number;
+        "react/destructuring-assignment": number;
+        "react/forbid-prop-types": number;
+        "react/jsx-closing-bracket-location": number;
+        "react/jsx-indent": string;
+        "react/jsx-newline": number;
+        "react/jsx-props-no-spreading": number;
+        "react/jsx-one-expression-per-line": number;
+        "react/no-deprecated": number;
+        "react/no-unused-class-component-methods": number;
+        "react/prefer-stateless-function": number;
+        "react/prop-types": number;
+        "react/require-default-props": number;
+        "react/require-extension": number;
+        "react/sort-comp": number;
     };
 };
 export default _default;
